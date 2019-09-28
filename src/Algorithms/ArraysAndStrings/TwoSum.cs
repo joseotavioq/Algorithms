81eb409035dda19ab8022f65159ce4a3f0597eb4
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-09-27 22:06:22 -0300

    Implemented TwoSum.

---
+++ src/Algorithms/ArraysAndStrings/TwoSum.cs
@@ -0,0 +1,82 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.ArraysAndStrings
+{
+    [MemoryDiagnoser]
+    public class TwoSum
+    {
+        [Benchmark(Baseline = true)]
+        [Arguments(new int[] { 0, 1, 2, 7, 11, 15 }, 9)]
+        public int[] FirstTry(int[] nums, int target)
+        {
+            if (nums == null || nums.Length < 2)
+                return null;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    int sum = nums[i] + nums[j];
+                    if (sum == target)
+                        return new int[] { i, j };
+                }
+            }
+
+            return null;
+        }
+
+        [Benchmark]
+        [Arguments(new int[] { 0, 1, 2, 7, 11, 15 }, 9)]
+        public int[] SecondTry(int[] nums, int target)
+        {
+            if (nums == null || nums.Length < 2)
+                return null;
+
+            int i = 0, j = 1;
+
+            while (true)
+            {
+                int sum = nums[i] + nums[j];
+
+                if (sum == target)
+                    return new int[] { i, j };
+                else if (j == nums.Length - 1)
+                {
+                    i++;
+                    j = i + 1;
+                }
+                else
+                    j++;
+
+                if (i == nums.Length - 1)
+                    break;
+            }
+
+            return null;
+        }
+
+        [Benchmark]
+        [Arguments(new int[] { 0, 1, 2, 7, 11, 15 }, 9)]
+        public int[] ThirdTry(int[] nums, int target)
+        {
+            if (nums == null || nums.Length < 2)
+                return null;
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (map.ContainsKey(diff) && map[diff] != i)
+                    return new int[] { map[diff], i };
+                else
+                    map.Add(nums[i], i);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/TwoSumTest.cs
@@ -0,0 +1,209 @@
+﻿using Algorithms.ArraysAndStrings;
+using Xunit;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class TwoSumTest
+    {
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_First()
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] result = new TwoSum().FirstTry(nums, target);
+
+            Assert.Equal(new int[] { 0, 1 }, result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_First_Unordered()
+        {
+            int[] nums = new int[] { 0, 4, 3, 0 };
+            int target = 0;
+
+            int[] result = new TwoSum().FirstTry(nums, target);
+
+            Assert.Equal(new int[] { 0, 3 }, result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_First_Another_Parameter()
+        {
+            int[] nums = new int[] { 0, 1, 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] result = new TwoSum().FirstTry(nums, target);
+
+            Assert.Equal(new int[] { 2, 3 }, result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_First_When_Does_Not_Have_A_Match()
+        {
+            int[] nums = new int[] { 0, 1, 2, 5, 11, 15 };
+            int target = 9;
+
+            int[] result = new TwoSum().FirstTry(nums, target);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Null_First_When_Passing_Null_Array()
+        {
+            int[] nums = null;
+            int target = 9;
+
+            int[] result = new TwoSum().FirstTry(nums, target);
+
+            Assert.Null(result);
+        }
+
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Null_First_When_Passing_Less_Than_Two_Items()
+        {
+            int[] nums = new int[] { 9 };
+            int target = 9;
+
+            int[] result = new TwoSum().FirstTry(nums, target);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_Second()
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] result = new TwoSum().SecondTry(nums, target);
+
+            Assert.Equal(new int[] { 0, 1 }, result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_Second_Unordered()
+        {
+            int[] nums = new int[] { 0, 4, 3, 0 };
+            int target = 0;
+
+            int[] result = new TwoSum().SecondTry(nums, target);
+
+            Assert.Equal(new int[] { 0, 3 }, result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_Second_Another_Parameter()
+        {
+            int[] nums = new int[] { 0, 1, 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] result = new TwoSum().SecondTry(nums, target);
+
+            Assert.Equal(new int[] { 2, 3 }, result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_Second_When_Does_Not_Have_A_Match()
+        {
+            int[] nums = new int[] { 0, 1, 2, 5, 11, 15 };
+            int target = 9;
+
+            int[] result = new TwoSum().SecondTry(nums, target);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Null_Second_When_Passing_Null_Array()
+        {
+            int[] nums = null;
+            int target = 9;
+
+            int[] result = new TwoSum().SecondTry(nums, target);
+
+            Assert.Null(result);
+        }
+
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Null_Second_When_Passing_Less_Than_Two_Items()
+        {
+            int[] nums = new int[] { 9 };
+            int target = 9;
+
+            int[] result = new TwoSum().SecondTry(nums, target);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_Third()
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] result = new TwoSum().ThirdTry(nums, target);
+
+            Assert.Equal(new int[] { 0, 1 }, result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_Third_Unordered()
+        {
+            int[] nums = new int[] { 0, 4, 3, 0 };
+            int target = 0;
+
+            int[] result = new TwoSum().ThirdTry(nums, target);
+
+            Assert.Equal(new int[] { 0, 3 }, result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_Third_Another_Parameter()
+        {
+            int[] nums = new int[] { 0, 1, 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] result = new TwoSum().ThirdTry(nums, target);
+
+            Assert.Equal(new int[] { 2, 3 }, result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Target_Third_When_Does_Not_Have_A_Match()
+        {
+            int[] nums = new int[] { 0, 1, 2, 5, 11, 15 };
+            int target = 9;
+
+            int[] result = new TwoSum().ThirdTry(nums, target);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Null_Third_When_Passing_Null_Array()
+        {
+            int[] nums = null;
+            int target = 9;
+
+            int[] result = new TwoSum().ThirdTry(nums, target);
+
+            Assert.Null(result);
+        }
+
+
+        [Fact]
+        public void Sum_Of_Two_Nums_Are_Equal_To_Null_Third_When_Passing_Less_Than_Two_Items()
+        {
+            int[] nums = new int[] { 9 };
+            int target = 9;
+
+            int[] result = new TwoSum().ThirdTry(nums, target);
+
+            Assert.Null(result);
+        }
+    }
+}
