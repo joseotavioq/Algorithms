de9962f6c76a140709dc2072331ab04be41c0d76
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-09-29 14:14:40 -0300

    Implemented URLify algorithm.

---
+++ src/Algorithms/ArraysAndStrings/URLify.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Algorithms.ArraysAndStrings
+{
+    [MemoryDiagnoser]
+    public class URLify
+    {
+        //Time: O(n)
+        //Space: O(n)
+        [Benchmark(Baseline = true)]
+        [Arguments("Mr John Smith    ")]
+        public string FirstTry(string value)
+        {
+            char[] urlIfied = new char[value.Length];
+            int j = 0;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] != ' ')
+                    urlIfied[j++] = value[i];
+                else
+                {
+                    urlIfied[j++] = '%';
+                    urlIfied[j++] = '2';
+                    urlIfied[j++] = '0';
+                }
+
+                if (j == value.Length)
+                    break;
+            }
+
+            return new string(urlIfied);
+        }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/URLifyTest.cs
@@ -0,0 +1,18 @@
+﻿using Algorithms.ArraysAndStrings;
+using Xunit;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class URLifyTest
+    {
+        [Fact]
+        public void FirstTry_URLify_The_Parameter()
+        {
+            string input = "Mr John Smith    ";
+
+            string output = new URLify().FirstTry(input);
+
+            Assert.Equal("Mr%20John%20Smith", output);
+        }
+    }
+}
