274880591420394ebc7f0820ade7f200d693dec9
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-09-30 11:18:22 -0300

    Added one more test method for URLify.

--- test/AlgorithmsTest/ArraysAndStrings/URLifyTest.cs
+++ test/AlgorithmsTest/ArraysAndStrings/URLifyTest.cs
@@ -14,5 +14,15 @@ namespace AlgorithmsTest.ArraysAndStrings
 
             Assert.Equal("Mr%20John%20Smith", output);
         }
+
+        [Fact]
+        public void FirstTry_With_Less_Spaces_Than_Necessary()
+        {
+            string input = "Mr John Smith   ";
+
+            string output = new URLify().FirstTry(input);
+
+            Assert.Equal("Mr%20John%20Smit", output);
+        }
     }
 }
