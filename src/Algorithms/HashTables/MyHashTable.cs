7f7c46db2effba3da11ef6a7cc82cc59fad3b4e3
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-11-15 17:16:45 -0300

    Implemented the HashTable data structure.

---
+++ src/Algorithms/HashTables/MyHashTable.cs
@@ -0,0 +1,82 @@
+﻿namespace Algorithms.HashTables
+{
+    public class MyHashTable<T>
+    {
+        private readonly Node<T>[] _data;
+
+        public MyHashTable(int size)
+        {
+            _data = new Node<T>[size];
+        }
+
+        private int generateHash(string key)
+        {
+            int hash = 0;
+
+            for (int i = 0; i < key.Length; i++)
+                hash = (hash + key[i] * i) % _data.Length;
+
+            return hash;
+        }
+
+        public T Get(string key)
+        {
+            int hash = generateHash(key);
+            return getOnBucket(hash, key);
+        }
+
+        private T getOnBucket(int hash, string key)
+        {
+            var current = _data[hash];
+            while (current != null)
+            {
+                if (current.Key == key)
+                    return current.Value;
+
+                current = current.Next;
+            }
+
+            return default(T);
+        }
+
+        public void Set(string key, T value)
+        {
+            int hash = generateHash(key);
+            setOnBucket(hash, key, value);
+        }
+
+        private void setOnBucket(int hash, string key, T value)
+        {
+            if (_data[hash] == null)
+                _data[hash] = new Node<T>(key, value);
+            else
+            {
+                var current = _data[hash];
+                while (current != null)
+                {
+                    if (current.Next == null)
+                    {
+                        current.Next = new Node<T>(key, value);
+                        break;
+                    }
+
+                    current = current.Next;
+                }
+            }
+        }
+    }
+
+    public class Node<T>
+    {
+        public Node(string key, T value)
+        {
+            Key = key;
+            Value = value;
+            Next = null;
+        }
+
+        public T Value { get; private set; }
+        public string Key { get; private set; }
+        public Node<T> Next { get; set; }
+    }
+}
---
+++ test/AlgorithmsTest/HashTables/MyHashTableTest.cs
@@ -0,0 +1,39 @@
+﻿using Algorithms.HashTables;
+using Xunit;
+
+namespace AlgorithmsTest.HashTables
+{
+    public class MyHashTableTest
+    {
+        [Fact]
+        public void Insert_An_Item_And_Get_It_Back()
+        {
+            var myHashTable = new MyHashTable<int>(10);
+            string key = "myTest";
+            int value = 1000;
+
+            myHashTable.Set(key, value);
+            int result = myHashTable.Get(key);
+
+            Assert.Equal(value, result);
+        }
+
+        [Fact]
+        public void Insert_Items_Testing_Colission()
+        {
+            var myHashTable = new MyHashTable<int>(10);
+            string key = "myTest";
+            string key2 = "myTest2";
+            string key3 = "myTest7";
+            int value = 5;
+
+            myHashTable.Set(key, 10);
+            myHashTable.Set(key2, 2);
+            myHashTable.Set(key3, value);
+
+            int result = myHashTable.Get(key3);
+
+            Assert.Equal(value, result);
+        }
+    }
+}
