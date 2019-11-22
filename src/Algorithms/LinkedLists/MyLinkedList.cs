23be867bdb9f104149691107f818aec701d0e17a
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-11-21 23:09:21 -0300

    Created MyLinkedList and added a method called Append.

---
+++ src/Algorithms/LinkedLists/MyLinkedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.LinkedLists
+{
+    public class MyLinkedList<T>
+    {
+        public Node<T> Head { get; private set; }
+        public Node<T> Tail { get; private set; }
+        public int Length { get; private set; }
+
+        public MyLinkedList(T value)
+        {
+            Head = new Node<T>(value);
+            Tail = Head;
+            Length = 1;
+        }
+
+        public void Append(T value)
+        {
+            var newNode = new Node<T>(value);
+            Tail.Next = newNode;
+            Tail = newNode;
+            Length++;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/LinkedLists/MyLinkedListTest.cs
@@ -0,0 +1,49 @@
+﻿using Algorithms.LinkedLists;
+using Xunit;
+
+namespace AlgorithmsTest.LinkedLists
+{
+    public class MyLinkedListTest
+    {
+        [Fact]
+        public void Create_My_Linked_List()
+        {
+            var myLinkedList = new MyLinkedList<int>(10);
+
+            Assert.Equal(10, myLinkedList.Head.Value);
+            Assert.Null(myLinkedList.Head.Next);
+            Assert.Equal(10, myLinkedList.Tail.Value);
+            Assert.Equal(1, myLinkedList.Length);
+        }
+
+        [Fact]
+        public void Append_An_Element()
+        {
+            var myLinkedList = new MyLinkedList<int>(10);
+
+            myLinkedList.Append(5);
+
+            Assert.Equal(10, myLinkedList.Head.Value);
+            Assert.Equal(5, myLinkedList.Head.Next.Value);
+            Assert.Null(myLinkedList.Head.Next.Next);
+            Assert.Equal(5, myLinkedList.Tail.Value);
+            Assert.Equal(2, myLinkedList.Length);
+        }
+
+        [Fact]
+        public void Append_Two_Elements()
+        {
+            var myLinkedList = new MyLinkedList<int>(10);
+
+            myLinkedList.Append(5);
+            myLinkedList.Append(16);
+
+            Assert.Equal(10, myLinkedList.Head.Value);
+            Assert.Equal(5, myLinkedList.Head.Next.Value);
+            Assert.Equal(16, myLinkedList.Head.Next.Next.Value);
+            Assert.Null(myLinkedList.Head.Next.Next.Next);
+            Assert.Equal(16, myLinkedList.Tail.Value);
+            Assert.Equal(3, myLinkedList.Length);
+        }
+    }
+}
