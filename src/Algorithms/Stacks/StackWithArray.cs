bdf6d7adcec7549b355aa2b88585bebe3bac01bc
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-11-23 13:53:27 -0300

    Added a Stack implemented with Array.

---
+++ src/Algorithms/Stacks/StackWithArray.cs
@@ -0,0 +1,53 @@
+﻿namespace Algorithms.Stacks
+{
+    public class StackWithArray<T>
+    {
+        private T[] _items;
+
+        public T Top { get { return Peek(); } }
+        public T Bottom { get { return _items[0]; } }
+        public int Length { get; private set; }
+
+        public StackWithArray()
+        {
+            _items = new T[5];
+        }
+
+        public T Peek()
+        {
+            if (Length == 0)
+                return default(T);
+
+            return _items[Length - 1];
+        }
+
+        public void Push(T value)
+        {
+            if(Length + 1 > _items.Length)
+            {
+                var newArray = new T[_items.Length * 2];
+                _items.CopyTo(newArray, 0);
+                _items = newArray;
+            }
+
+            _items[Length] = value;
+            Length++;
+        }
+
+        public T Pop()
+        {
+            if (Length == 0)
+                return default(T);
+
+            var item = _items[Length - 1];
+            _items[Length - 1] = default(T);
+            Length--;
+            return item;
+        }
+
+        public bool IsEmpty()
+        {
+            return Length == 0;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Stacks/StackWithArrayTest.cs
@@ -0,0 +1,196 @@
+﻿using Algorithms.Stacks;
+using Xunit;
+
+namespace AlgorithmsTest.Stacks
+{
+    public class StackWithArrayTest
+    {
+        [Fact]
+        public void Stack_Is_Not_Empty()
+        {
+            var stack = new StackWithArray<int>();
+
+            stack.Push(10);
+            var isEmpty = stack.IsEmpty();
+
+            Assert.False(isEmpty);
+            Assert.Equal(1, stack.Length);
+            Assert.Equal(10, stack.Top);
+            Assert.Equal(10, stack.Bottom);
+        }
+
+        [Fact]
+        public void Stack_Is_Empty()
+        {
+            var stack = new StackWithArray<int>();
+
+            var isEmpty = stack.IsEmpty();
+
+            Assert.True(isEmpty);
+            Assert.Equal(0, stack.Length);
+            Assert.Equal(0, stack.Top);
+            Assert.Equal(0, stack.Bottom);
+        }
+
+        [Fact]
+        public void Push_An_Element()
+        {
+            var stack = new StackWithArray<int>();
+
+            stack.Push(10);
+
+            Assert.Equal(1, stack.Length);
+            Assert.Equal(10, stack.Top);
+            Assert.Equal(10, stack.Bottom);
+        }
+
+        [Fact]
+        public void Push_Two_Elements()
+        {
+            var stack = new StackWithArray<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+
+            Assert.Equal(2, stack.Length);
+            Assert.Equal(20, stack.Top);
+            Assert.Equal(10, stack.Bottom);
+        }
+
+        [Fact]
+        public void Push_Three_Elements()
+        {
+            var stack = new StackWithArray<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            Assert.Equal(3, stack.Length);
+            Assert.Equal(30, stack.Top);
+            Assert.Equal(10, stack.Bottom);
+        }
+
+        [Fact]
+        public void Push_Nine_Elements()
+        {
+            var stack = new StackWithArray<int>();
+
+            for (int i = 1; i < 10; i++)
+            {
+                stack.Push(i);
+            }
+
+            Assert.Equal(9, stack.Length);
+            Assert.Equal(9, stack.Top);
+            Assert.Equal(1, stack.Bottom);
+        }
+
+        [Fact]
+        public void Pop_An_Element_In_A_Stack_With_One_Element()
+        {
+            var stack = new StackWithArray<int>();
+
+            stack.Push(20);
+
+            var item = stack.Pop();
+
+            Assert.Equal(20, item);
+            Assert.Equal(0, stack.Length);
+            Assert.Equal(0, stack.Top);
+            Assert.Equal(0, stack.Bottom);
+        }
+
+        [Fact]
+        public void Pop_An_Element_In_A_Stack_With_Two_Elements()
+        {
+            var stack = new StackWithArray<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+
+            var item = stack.Pop();
+
+            Assert.Equal(20, item);
+            Assert.Equal(1, stack.Length);
+            Assert.Equal(10, stack.Top);
+            Assert.Equal(10, stack.Bottom);
+        }
+
+        [Fact]
+        public void Pop_Two_Elements_In_A_Stack_With_Three_Elements()
+        {
+            var stack = new StackWithArray<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            var item1 = stack.Pop();
+            var item2 = stack.Pop();
+
+            Assert.Equal(30, item1);
+            Assert.Equal(20, item2);
+            Assert.Equal(1, stack.Length);
+            Assert.Equal(10, stack.Top);
+            Assert.Equal(10, stack.Bottom);
+        }
+
+        [Fact]
+        public void Pop_An_Element_In_A_Stack_With_No_Element()
+        {
+            var stack = new StackWithArray<int>();
+
+            var item = stack.Pop();
+
+            Assert.Equal(0, item);
+            Assert.Equal(0, stack.Length);
+            Assert.Equal(0, stack.Top);
+            Assert.Equal(0, stack.Bottom);
+        }
+
+        [Fact]
+        public void Peek_Element_In_A_Stack_With_Two_Elements()
+        {
+            var stack = new StackWithArray<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+
+            var item = stack.Peek();
+
+            Assert.Equal(20, item);
+            Assert.Equal(2, stack.Length);
+            Assert.Equal(20, stack.Top);
+            Assert.Equal(10, stack.Bottom);
+        }
+
+        [Fact]
+        public void Peek_Element_In_A_Stack_With_One_Element()
+        {
+            var stack = new StackWithArray<int>();
+
+            stack.Push(20);
+
+            var item = stack.Peek();
+
+            Assert.Equal(20, item);
+            Assert.Equal(1, stack.Length);
+            Assert.Equal(20, stack.Top);
+            Assert.Equal(20, stack.Bottom);
+        }
+
+        [Fact]
+        public void Peek_An_Element_In_A_Stack_With_No_Element()
+        {
+            var stack = new StackWithArray<int>();
+
+            var item = stack.Peek();
+
+            Assert.Equal(0, item);
+            Assert.Equal(0, stack.Length);
+            Assert.Equal(0, stack.Top);
+            Assert.Equal(0, stack.Bottom);
+        }
+    }
+}
