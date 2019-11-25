4a72d6d58f5189b3ea0b5c5989454229d7f7d9bc
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-11-24 21:30:47 -0300

    Implemented SumLists algorithm.

---
+++ src/Algorithms/LinkedLists/SumLists.cs
@@ -0,0 +1,84 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.LinkedLists
+{
+    //[MemoryDiagnoser]
+    public class SumLists
+    {
+        //public IEnumerable<MyLinkedList<int>> LinkedLists()
+        //{
+        //    var first = new MyLinkedList<int>(7);
+        //    first.Append(1);
+        //    first.Append(6);
+
+        //    yield return first;
+        //}
+
+        //[Benchmark(Baseline = true)]
+        //[ArgumentsSource(nameof(LinkedLists))]
+        public Node<int> FirstTry(MyLinkedList<int> first, MyLinkedList<int> second)
+        {
+            int firstNumber = GetNumber(first.Head, first.Length);
+            int secondNumber = GetNumber(second.Head, second.Length);
+            int sum = firstNumber + secondNumber;
+
+            var result = GenerateLinkedList(sum);
+
+            return result;
+        }
+
+        private Node<int> GenerateLinkedList(int sum)
+        {
+            Node<int> head = null;
+
+            decimal number = 0;
+
+            Node<int> current = null;
+            while (sum > 0)
+            {
+                number = (decimal)sum / 10;
+                sum = (int)number;
+
+                int digit = (int)((number - sum) * 10);
+
+                if (head == null)
+                {
+                    head = new Node<int>(digit);
+                    current = head;
+                }
+                else
+                {
+                    current.Next = new Node<int>(digit);
+                    current = current.Next;
+                }
+            }
+
+            return head;
+        }
+
+        private int GetNumber(Node<int> head, int length)
+        {
+            decimal number = 0;
+
+            var current = head;
+            while (current != null)
+            {
+                number += current.Value;
+                number /= 10;
+
+                current = current.Next;
+            }
+
+            int multiply = 10;
+            for (int i = 1; i < length; i++)
+            {
+                multiply *= 10;
+            }
+
+            return (int)(number * multiply);
+        }
+    }
+}
---
+++ test/AlgorithmsTest/LinkedLists/SumListsTest.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.LinkedLists;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AlgorithmsTest.LinkedLists
+{
+    public class SumListsTest
+    {
+        [Fact]
+        public void Sum_Two_Lists()
+        {
+            var first = new MyLinkedList<int>(7);
+            first.Append(1);
+            first.Append(6);
+
+            var second = new MyLinkedList<int>(5);
+            second.Append(9);
+            second.Append(2);
+
+            var result = new SumLists().FirstTry(first, second);
+
+            Assert.Equal(2, result.Value);
+            Assert.Equal(1, result.Next.Value);
+            Assert.Equal(9, result.Next.Next.Value);
+            Assert.Null(result.Next.Next.Next);
+        }
+    }
+}
