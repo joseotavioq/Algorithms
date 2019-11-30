8ae0592f7c5227222d6b9c31428960b422b6aeac
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-11-30 16:42:58 -0300

    Implemented Sorted Stack.

---
+++ src/Algorithms/Stacks/StackSorted.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithms.Stacks
+{
+    public class StackSorted : StackWithLinkedList<int>
+    {
+        public override void Push(int value)
+        {
+            base.Push(value);
+
+            var sorted = new StackWithLinkedList<int>();
+
+            while (!base.IsEmpty())
+            {
+                int element = base.Pop();
+                while(!sorted.IsEmpty() && sorted.Peek() > element)
+                {
+                    base.Push(sorted.Pop());
+                }
+
+                sorted.Push(element);
+            }
+
+            while(!sorted.IsEmpty())
+            {
+                base.Push(sorted.Pop());
+            }
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Stacks/StackSortedTest.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms.Stacks;
+using Xunit;
+
+namespace AlgorithmsTest.Stacks
+{
+    public class StackSortedTest
+    {
+        [Fact]
+        public void Push_Three_Elements()
+        {
+            var stack = new StackSorted();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            Assert.Equal(3, stack.Length);
+            Assert.Equal(10, stack.Top.Value);
+            Assert.Equal(30, stack.Bottom.Value);
+        }
+    }
+}
