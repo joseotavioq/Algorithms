42931794d7a71d7b4697c3f0e3c7bcb57ba3bfdf
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-12-02 23:06:16 -0300

    MyUndirectedGraph implemented.

---
+++ src/Algorithms/Graph/MyUndirectedGraph.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Graph
+{
+    /// <summary>
+    /// Adjacent Cyclic Unweighted and Undirected Graph
+    /// </summary>
+    public class MyUndirectedGraph
+    {
+        public int NumbersOfNodes { get { return _adjacentList.Count; } }
+
+        private Dictionary<int, int[]> _adjacentList = new Dictionary<int, int[]>();
+
+        public void AddVertex(int node)
+        {
+            _adjacentList.Add(node, null);
+        }
+
+        public void AddEdge(int node1, int node2)
+        {
+            addItem(node1, node2);
+            addItem(node2, node1);
+        }
+
+        private void addItem(int node1, int node2)
+        {
+            int[] value = _adjacentList[node1] as int[];
+            if (value == null)
+            {
+                _adjacentList[node1] = new int[1] { node2 };
+            }
+            else
+            {
+                var newValue = new int[value.Length + 1];
+
+                for (int i = 0; i < value.Length; i++)
+                {
+                    newValue[i] = value[i];
+                }
+
+                newValue[value.Length] = node2;
+
+                _adjacentList[node1] = newValue;
+            }
+        }
+
+        public int[] GetVertex(int node)
+        {
+            return _adjacentList[node];
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Graph/MyUndirectedGraphTest.cs
@@ -0,0 +1,35 @@
+﻿using Algorithms.Graph;
+using Xunit;
+
+namespace AlgorithmsTest.Graph
+{
+    public class MyUndirectedGraphTest
+    {
+        [Fact]
+        public void Create_A_Graph_With_Connections()
+        {
+            var graph = new MyUndirectedGraph();
+
+            for (int i = 0; i <= 6; i++)
+                graph.AddVertex(i);
+
+            graph.AddEdge(3, 1);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(4, 2);
+            graph.AddEdge(4, 5);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 0);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(6, 5);
+
+            Assert.Equal(7, graph.NumbersOfNodes);
+            Assert.Equal(new int[2] { 1, 2 }, graph.GetVertex(0));
+            Assert.Equal(new int[3] { 3, 2, 0 }, graph.GetVertex(1));
+            Assert.Equal(new int[3] { 4, 1, 0 }, graph.GetVertex(2));
+            Assert.Equal(new int[2] { 1, 4 }, graph.GetVertex(3));
+            Assert.Equal(new int[3] { 3, 2, 5 }, graph.GetVertex(4));
+            Assert.Equal(new int[2] { 4, 6 }, graph.GetVertex(5));
+            Assert.Equal(new int[1] { 5 }, graph.GetVertex(6));
+        }
+    }
+}
