21b8f0763e727601b601fa13236ca625b294c61c
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-12-04 22:23:08 -0300

    Adding some comments to make it more clear.

--- test/AlgorithmsTest/Graph/MyUndirectedGraphTest.cs
+++ test/AlgorithmsTest/Graph/MyUndirectedGraphTest.cs
@@ -10,17 +10,19 @@ namespace AlgorithmsTest.Graph
         {
             var graph = new MyUndirectedGraph();
 
+            //Create all the vertex
             for (int i = 0; i <= 6; i++)
                 graph.AddVertex(i);
 
-            graph.AddEdge(3, 1);
-            graph.AddEdge(3, 4);
-            graph.AddEdge(4, 2);
-            graph.AddEdge(4, 5);
-            graph.AddEdge(1, 2);
-            graph.AddEdge(1, 0);
-            graph.AddEdge(0, 2);
-            graph.AddEdge(6, 5);
+            //Link the Vertexes
+            graph.AddEdge(3, 1); //3 -> 1 && 1 -> 3
+            graph.AddEdge(3, 4); //3 -> 4 && 4 -> 3
+            graph.AddEdge(4, 2); //4 -> 2 && 2 -> 4
+            graph.AddEdge(4, 5); //4 -> 5 && 5 -> 4
+            graph.AddEdge(1, 2); //1 -> 2 && 2 -> 1
+            graph.AddEdge(1, 0); //1 -> 0 && 0 -> 1
+            graph.AddEdge(0, 2); //0 -> 2 && 2 -> 0
+            graph.AddEdge(6, 5); //6 -> 5 && 5 -> 6
 
             Assert.Equal(7, graph.NumbersOfNodes);
             Assert.Equal(new int[2] { 1, 2 }, graph.GetVertex(0));
