f1283a184a7d7f407f7b8dcb44dd792ee427669a
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-01-09 21:42:04 -0300

    Recursion for Fibonacci implemented.

---
+++ src/Algorithms/Recursion/Fibonacci.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Recursion
+{
+    [MemoryDiagnoser]
+    public class Fibonacci
+    {
+        [Benchmark]
+        [Arguments(30)]
+        public int Recursive(int index)
+        {
+            if (index < 2)
+                return index;
+
+            return Recursive(index - 1) + Recursive(index - 2);
+        }
+
+        [Benchmark]
+        [Arguments(30)]
+        public int Iterative(int index)
+        {
+            List<int> elements = new List<int>() { 0, 1 };
+
+            for (int i = 2; i <= index; i++)
+            {
+                elements.Add(elements[i - 2] + elements[i - 1]);
+            }
+
+            return elements[index];
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Recursion/FibonacciTest.cs
@@ -0,0 +1,104 @@
+﻿using Algorithms.Recursion;
+using Xunit;
+
+namespace AlgorithmsTest.Recursion
+{
+    public class FibonacciTest
+    {
+        [Fact]
+        public void Iterative_Index_of_10()
+        {
+            var result = new Fibonacci().Iterative(10);
+
+            Assert.Equal(55, result);
+        }
+
+        [Fact]
+        public void Iterative_Index_of_7()
+        {
+            var result = new Fibonacci().Iterative(7);
+
+            Assert.Equal(13, result);
+        }
+
+        [Fact]
+        public void Iterative_Index_of_3()
+        {
+            var result = new Fibonacci().Iterative(3);
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Iterative_Index_of_2()
+        {
+            var result = new Fibonacci().Iterative(2);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Iterative_Index_of_1()
+        {
+            var result = new Fibonacci().Iterative(1);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Iterative_Index_of_0()
+        {
+            var result = new Fibonacci().Iterative(0);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Recursive_Index_of_10()
+        {
+            var result = new Fibonacci().Recursive(10);
+
+            Assert.Equal(55, result);
+        }
+
+        [Fact]
+        public void Recursive_Index_of_7()
+        {
+            var result = new Fibonacci().Recursive(7);
+
+            Assert.Equal(13, result);
+        }
+
+        [Fact]
+        public void Recursive_Index_of_3()
+        {
+            var result = new Fibonacci().Recursive(3);
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Recursive_Index_of_2()
+        {
+            var result = new Fibonacci().Recursive(2);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Recursive_Index_of_1()
+        {
+            var result = new Fibonacci().Recursive(1);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Recursive_Index_of_0()
+        {
+            var result = new Fibonacci().Recursive(0);
+
+            Assert.Equal(0, result);
+        }
+    }
+}
