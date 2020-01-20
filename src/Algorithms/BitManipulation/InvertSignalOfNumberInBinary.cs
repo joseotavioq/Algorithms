8d66dcce4f939a0654e0b27676a20a38bf9478be
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-01-20 19:49:37 -0300

    Added an method to return a negative binary representation of the input number.

---
+++ src/Algorithms/BitManipulation/InvertSignalOfNumberInBinary.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms.BitManipulation
+{
+    public class InvertSignalOfNumberInBinary
+    {
+        public byte[] FirstTry(sbyte number)
+        {
+            var convertToBinary = new ConvertNumberToBinary().FirstTry(number);
+
+            for (int i = 0; i < convertToBinary.Length; i++)
+            {
+                convertToBinary[i] = (byte)((convertToBinary[i] == 1) ? 0 : 1);
+            }
+
+            for (int i = convertToBinary.Length - 1; i >= 0; i--)
+            {
+                if (convertToBinary[i] == 0)
+                {
+                    convertToBinary[i] = 1;
+                    break;
+                }
+                else
+                {
+                    convertToBinary[i] = 0;
+                }
+            }
+
+            return convertToBinary;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/BitManipulation/InvertSignalOfNumberInBinaryTest.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.BitManipulation;
+using Xunit;
+
+namespace AlgorithmsTest.BitManipulation
+{
+    public class InvertSignalOfNumberInBinaryTest
+    {
+        [Fact]
+        public void Convert_Signal_Of_18_To_Negative_In_Binary()
+        {
+            sbyte number = 18;
+
+            byte[] result = new InvertSignalOfNumberInBinary().FirstTry(number);
+
+            //-18
+            Assert.Equal(new byte[] { 1, 1, 1, 0, 1, 1, 1, 0 }, result);
+        }
+
+        [Fact]
+        public void Convert_Signal_Of_123_To_Negative_In_Binary()
+        {
+            sbyte number = 123;
+
+            byte[] result = new InvertSignalOfNumberInBinary().FirstTry(number);
+
+            //-123
+            Assert.Equal(new byte[] { 1, 0, 0, 0, 0, 1, 0, 1 }, result);
+        }
+    }
+}
