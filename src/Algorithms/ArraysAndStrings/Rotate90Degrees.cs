09d22a42dd1145464c2975687bd7794c3cab8b7c
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-02-11 20:18:48 -0300

    Implemented rotate 90 degrees in a 2-dimensional array.

---
+++ src/Algorithms/ArraysAndStrings/Rotate90Degrees.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms.ArraysAndStrings
+{
+    public class Rotate90Degrees
+    {
+        public string[,] FirstTry(string[,] contents, int n)
+        {
+            string[,] newContent = new string[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int posi = n - j - 1;
+                    newContent[i, j] = contents[posi, i];
+                }
+            }
+
+            return newContent;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/Rotate90DegreesTest.cs
@@ -0,0 +1,38 @@
+﻿using Algorithms.ArraysAndStrings;
+using Xunit;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class Rotate90DegreesTest
+    {
+        [Fact]
+        public void FirstTry_Rotate_3_3_Array()
+        {
+            string[,] contents = new string[,] { { "Aqua", "Black", "Blue" } ,
+                                                 { "Fuschia", "Gray", "Green" } ,
+                                                 { "Lime", "Maroon", "Navy" } };
+
+            string[,] result = new Rotate90Degrees().FirstTry(contents, 3);
+
+            Assert.Equal(new string[,] { { "Lime", "Fuschia", "Aqua" },
+                                         { "Maroon", "Gray", "Black"},
+                                         { "Navy", "Green", "Blue"} }, result);
+        }
+
+        [Fact]
+        public void FirstTry_Rotate_4_4_Array()
+        {
+            string[,] contents = new string[,] { { "Aqua", "Black", "Blue", "Yellow" } ,
+                                                 { "Fuschia", "Gray", "Green", "White" } ,
+                                                 { "Lime", "Maroon", "Navy", "Red" },
+                                                 { "Gold", "Silver", "Purple", "Pink" } };
+
+            string[,] result = new Rotate90Degrees().FirstTry(contents, 4);
+
+            Assert.Equal(new string[,] { { "Gold", "Lime", "Fuschia", "Aqua" },
+                                         { "Silver", "Maroon", "Gray", "Black"},
+                                         { "Purple", "Navy", "Green", "Blue"},
+                                         { "Pink", "Red", "White", "Yellow"}}, result);
+        }
+    }
+}
