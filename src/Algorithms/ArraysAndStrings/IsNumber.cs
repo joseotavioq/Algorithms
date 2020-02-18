adc017b99e5faf5abd5a2db050008d0d0df2006a
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-02-18 20:31:43 -0300

    Implemented IsNumber algorithm

---
+++ src/Algorithms/ArraysAndStrings/IsNumber.cs
@@ -0,0 +1,54 @@
+﻿namespace Algorithms.ArraysAndStrings
+{
+    public class IsNumber
+    {
+        public bool FirstTry(string s)
+        {
+            int start = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ')
+                {
+                    start = i;
+                    break;
+                }
+            }
+
+            int end = s.Length;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    end = i + 1;
+                    break;
+                }
+            }
+
+            int length = end - start;
+
+            if (length <= 0)
+                return false;
+
+            bool signAppeared = false;
+            bool decimalPointAppeared = false;
+            for (int i = start; i < end; i++)
+            {
+                bool isNumber = (s[i] >= '0' && s[i] <= '9');
+                bool isExpoent = (s[i] == 'e' && length > 2 && (i - 1 >= 0 && s[i - 1] >= '0' && s[i - 1] <= '9') && (i == end - 3 || i == end - 2));
+                bool isNegativeOrPositiveSign = ((s[i] == '-' || s[i] == '+') && !signAppeared);
+                bool isDecimalPoint = (s[i] == '.' && length > 1 && !decimalPointAppeared);
+
+                if (isNegativeOrPositiveSign)
+                    signAppeared = true;
+
+                if (isDecimalPoint)
+                    decimalPointAppeared = true;
+
+                if (!isNumber && !isExpoent && !isNegativeOrPositiveSign && !isDecimalPoint)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/IsNumberTest.cs
@@ -0,0 +1,188 @@
+﻿using Algorithms.ArraysAndStrings;
+using Xunit;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class IsNumberTest
+    {
+        [Fact]
+        public void FirstTry_Single_Zero()
+        {
+            string myString = "0";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void FirstTry_Space_And_Decimal_Inside()
+        {
+            string myString = " 0.1 ";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void FirstTry_Space_And_Decimal_Point()
+        {
+            string myString = " .";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FirstTry_Only_Letters()
+        {
+            string myString = "abc";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FirstTry_A_Number_And_a_Letter()
+        {
+            string myString = "1 a";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FirstTry_A_Number_With_Exponent()
+        {
+            string myString = "2e10";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void FirstTry_A_Negative_Number_With_Exponent_And_Spaces()
+        {
+            string myString = " -90e3   ";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void FirstTry_An_Incorrect_Exponent_And_Spaces()
+        {
+            string myString = " 1e";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FirstTry_A_Correct_Exponent()
+        {
+            string myString = "2e0";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void FirstTry_An_Incorrect_Exponent()
+        {
+            string myString = "e3";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FirstTry_Another_Incorrect_Exponent()
+        {
+            string myString = ".e1";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FirstTry_Another_Incorrect_Exponent2()
+        {
+            string myString = "e11";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FirstTry_A_Number_With_Negative_Exponent_And_Spaces()
+        {
+            string myString = " 6e-1";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void FirstTry_A_Number_With_Incorrect_Exponent_And_Spaces()
+        {
+            string myString = " 99e2.5 ";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FirstTry_A_Number_Exponent()
+        {
+            string myString = "53.5e93";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void FirstTry_Two_Negative_Signs_And_Spaces()
+        {
+            string myString = " --6 ";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FirstTry_Two_Negative_Signs()
+        {
+            string myString = "-+3";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FirstTry_Incorrect_Number()
+        {
+            string myString = "95a54e53";
+
+            bool result = new IsNumber().FirstTry(myString);
+
+            Assert.False(result);
+        }
+    }
+}
