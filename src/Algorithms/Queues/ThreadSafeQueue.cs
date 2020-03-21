d40be5c84530c723a65dc4fdf4b51c73bc75b708
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-03-20 21:04:46 -0300

    Implemented ThreadSafeQueue.

---
+++ src/Algorithms/Queues/ThreadSafeQueue.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Queues
+{
+    public class ThreadSafeQueue<T>
+    {
+        private static object _lock = new object();
+        private Queue<T> _internalQueue = new Queue<T>();
+        private int _maxSize;
+
+        public ThreadSafeQueue(int maxSize)
+        {
+            _maxSize = maxSize;
+        }
+
+        public bool IsEmpty()
+        {
+            return _internalQueue.Count == 0;
+        }
+
+        public bool IsFull()
+        {
+            return _internalQueue.Count == _maxSize;
+        }
+
+        public void Enqueue(T value)
+        {
+            while (true)
+            {
+                if (!IsFull())
+                {
+                    lock (_lock)
+                    {
+                        if (!IsFull())
+                        {
+                            _internalQueue.Enqueue(value);
+                            return;
+                        }
+                    }
+                }
+
+                System.Threading.Thread.SpinWait(10);
+            }
+        }
+
+        public T Dequeue()
+        {
+            while (true)
+            {
+                if (!IsEmpty())
+                {
+                    lock (_lock)
+                    {
+                        if (!IsEmpty())
+                        {
+                            return _internalQueue.Dequeue();
+                        }
+                    }
+                }
+
+                System.Threading.Thread.SpinWait(10);
+            }
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Queues/ThreadSafeQueueTest.cs
@@ -0,0 +1,84 @@
+﻿using Algorithms.Queues;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlgorithmsTest.Queues
+{
+    public class ThreadSafeQueueTest
+    {
+        [Fact]
+        public void Add_Element_On_Queue_Until_Full_It()
+        {
+            ThreadSafeQueue<int> threadSafeQueue = new ThreadSafeQueue<int>(5);
+
+            threadSafeQueue.Enqueue(1);
+            threadSafeQueue.Enqueue(2);
+            threadSafeQueue.Enqueue(3);
+            threadSafeQueue.Enqueue(4);
+            threadSafeQueue.Enqueue(5);
+
+            Assert.False(threadSafeQueue.IsEmpty());
+            Assert.True(threadSafeQueue.IsFull());
+        }
+
+        [Fact]
+        public void Add_Element_On_Queue()
+        {
+            ThreadSafeQueue<int> threadSafeQueue = new ThreadSafeQueue<int>(5);
+
+            threadSafeQueue.Enqueue(1);
+
+            Assert.False(threadSafeQueue.IsEmpty());
+            Assert.False(threadSafeQueue.IsFull());
+        }
+
+        [Fact]
+        public void Empty_Queue()
+        {
+            ThreadSafeQueue<int> threadSafeQueue = new ThreadSafeQueue<int>(5);
+
+            Assert.True(threadSafeQueue.IsEmpty());
+            Assert.False(threadSafeQueue.IsFull());
+        }
+
+        [Fact]
+        public void Enqueue_Then_Dequeue_An_Element()
+        {
+            int elementToAdd = 1;
+            ThreadSafeQueue<int> threadSafeQueue = new ThreadSafeQueue<int>(1);
+
+            threadSafeQueue.Enqueue(elementToAdd);
+            var element = threadSafeQueue.Dequeue();
+
+            Assert.True(threadSafeQueue.IsEmpty());
+            Assert.False(threadSafeQueue.IsFull());
+            Assert.Equal(elementToAdd, element);
+        }
+
+        static ThreadSafeQueue<int> multiThreadQueue = new ThreadSafeQueue<int>(1);
+
+        [Fact]
+        public void MultiThread_Scenario()
+        {
+            int elementToAdd = 1;
+
+            Task dequeueTask = Task.Run(() =>
+            {
+                var value = multiThreadQueue.Dequeue();
+                Assert.Equal(elementToAdd, value);
+            });
+
+            Task waitTask = Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(500);
+            });
+
+            Task enqueueTask = Task.Run(() =>
+            {
+                multiThreadQueue.Enqueue(elementToAdd);
+            });
+
+            Task.WaitAll(dequeueTask, waitTask, enqueueTask);
+        }
+    }
+}
