eede690319f9df064fa6dbb544e914845e802259
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-03-25 09:37:55 -0300

    Merge Two Linked Lists implemented.

---
+++ src/Algorithms/LinkedLists/MergeTwoLinkedLists.cs
@@ -0,0 +1,77 @@
+﻿namespace Algorithms.LinkedLists
+{
+    /// <summary>
+    /// The parameters needed to be sorted in an ascending order.
+    /// </summary>
+    public class MergeTwoLinkedLists
+    {
+        public Node<int> FirstTry(Node<int> first, Node<int> second)
+        {
+            if (first == null)
+                return second;
+
+            if (second == null)
+                return first;
+
+            Node<int> head = null;
+            Node<int> current = null;
+
+            if (first.Value < second.Value)
+            {
+                current = first;
+                first = first.Next;
+            }
+            else
+            {
+                current = second;
+                second = second.Next;
+            }
+
+            head = current;
+
+            while (first != null || second != null)
+            {
+                if (first == null)
+                {
+                    current.Next = second;
+                    second = null;
+                }
+                else if (second == null)
+                {
+                    current.Next = first;
+                    first = null;
+                }
+                else if (current.Value == first.Value)
+                {
+                    first = first.Next;
+                    continue;
+                }
+                else if (current.Value == second.Value)
+                {
+                    second = second.Next;
+                    continue;
+                }
+                else if (first.Value < second.Value)
+                {
+                    current.Next = first;
+                    first = first.Next;
+                }
+                else if (first.Value > second.Value)
+                {
+                    current.Next = second;
+                    second = second.Next;
+                }
+                else
+                {
+                    current.Next = first;
+                    first = first.Next;
+                    second = second.Next;
+                }
+
+                current = current.Next;
+            }
+
+            return head;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/LinkedLists/MergeTwoLinkedListsTest.cs
@@ -0,0 +1,91 @@
+﻿using Algorithms.LinkedLists;
+using Xunit;
+
+namespace AlgorithmsTest.LinkedLists
+{
+    public class MergeTwoLinkedListsTest
+    {
+        [Fact]
+        public void Merge_Simple_LinkedLists()
+        {
+            var first = new Node<int>(2)
+            {
+                Next = new Node<int>(4)
+                {
+                    Next = new Node<int>(5)
+                    {
+                        Next = new Node<int>(7)
+                    }
+                }
+            };
+
+            var second = new Node<int>(1)
+            {
+                Next = new Node<int>(3)
+                {
+                    Next = new Node<int>(5)
+                    {
+                        Next = new Node<int>(6)
+                    }
+                }
+            };
+
+            var merged = new MergeTwoLinkedLists().FirstTry(first, second);
+
+            Assert.Equal(1, merged.Value);
+            Assert.Equal(2, merged.Next.Value);
+            Assert.Equal(3, merged.Next.Next.Value);
+            Assert.Equal(4, merged.Next.Next.Next.Value);
+            Assert.Equal(5, merged.Next.Next.Next.Next.Value);
+            Assert.Equal(6, merged.Next.Next.Next.Next.Next.Value);
+            Assert.Equal(7, merged.Next.Next.Next.Next.Next.Next.Value);
+            Assert.Null(merged.Next.Next.Next.Next.Next.Next.Next);
+        }
+
+        [Fact]
+        public void First_LinkedList_Is_Null()
+        {
+            var second = new Node<int>(1)
+            {
+                Next = new Node<int>(3)
+                {
+                    Next = new Node<int>(5)
+                    {
+                        Next = new Node<int>(6)
+                    }
+                }
+            };
+
+            var merged = new MergeTwoLinkedLists().FirstTry(null, second);
+
+            Assert.Equal(1, merged.Value);
+            Assert.Equal(3, merged.Next.Value);
+            Assert.Equal(5, merged.Next.Next.Value);
+            Assert.Equal(6, merged.Next.Next.Next.Value);
+            Assert.Null(merged.Next.Next.Next.Next);
+        }
+
+        [Fact]
+        public void Second_LinkedList_Is_Null()
+        {
+            var first = new Node<int>(2)
+            {
+                Next = new Node<int>(4)
+                {
+                    Next = new Node<int>(5)
+                    {
+                        Next = new Node<int>(7)
+                    }
+                }
+            };
+
+            var merged = new MergeTwoLinkedLists().FirstTry(first, null);
+
+            Assert.Equal(2, merged.Value);
+            Assert.Equal(4, merged.Next.Value);
+            Assert.Equal(5, merged.Next.Next.Value);
+            Assert.Equal(7, merged.Next.Next.Next.Value);
+            Assert.Null(merged.Next.Next.Next.Next);
+        }
+    }
+}
