d8aaba6f30fe9d7c6e218db6c9a8e7b2c9b20a3f
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-04-05 22:32:01 -0300

    Serializer and Deserializer of a Binary Tree.

---
+++ src/Algorithms/Tree/BinaryTreeConverter.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace Algorithms.Tree
+{
+    public class BinaryTreeConverter
+    {
+        public string Serialize(TreeNode<int> tree)
+        {
+            var sb = new StringBuilder();
+
+            RecursiveBuildString(tree, sb);
+
+            return sb.ToString();
+        }
+
+        private void RecursiveBuildString(TreeNode<int> tree, StringBuilder sb)
+        {
+            if (tree == null)
+            {
+                sb.Append("null-");
+            }
+            else
+            {
+                sb.Append(tree.Value);
+                sb.Append("-");
+
+                RecursiveBuildString(tree.Left, sb);
+
+                RecursiveBuildString(tree.Right, sb);
+            }
+        }
+
+        public TreeNode<int> Deserialize(string mystring)
+        {
+            if (string.IsNullOrEmpty(mystring) || mystring == "null-")
+                return null;
+
+            TreeNode<int> head = null;
+
+            var child = mystring.Split('-');
+
+            if (child.Length > 0)
+            {
+                int position = 0;
+                head = AddElement(child, ref position);
+            }
+
+            return head;
+        }
+
+        private TreeNode<int> AddElement(string[] child, ref int position)
+        {
+            if (position >= child.Length || child[position] == "null")
+                return null;
+
+            var item = new TreeNode<int>(int.Parse(child[position]));
+
+            position++;
+            item.Left = AddElement(child, ref position);
+
+            position++;
+            item.Right = AddElement(child, ref position);
+
+            return item;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Tree/BinaryTreeConverterTest.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+using Algorithms.Tree;
+
+namespace AlgorithmsTest.Tree
+{
+    public class BinaryTreeConverterTest
+    {
+        [Fact]
+        public void Serialize_BinaryTree()
+        {
+            var tree = new TreeNode<int>(6)
+            {
+                Left = new TreeNode<int>(5)
+                {
+                    Left = new TreeNode<int>(4)
+                },
+                Right = new TreeNode<int>(8)
+            };
+
+            var result = new BinaryTreeConverter().Serialize(tree);
+
+            Assert.Equal("6-5-4-null-null-null-8-null-null-", result);
+        }
+
+        [Fact]
+        public void Deserialize_BinaryTree()
+        {
+            var inputString = "6-5-4-null-null-null-8-null-null-";
+
+            var result = new BinaryTreeConverter().Deserialize(inputString);
+
+            Assert.Equal(6, result.Value);
+            Assert.Equal(5, result.Left.Value);
+            Assert.Equal(8, result.Right.Value);
+            Assert.Null(result.Right.Left);
+            Assert.Null(result.Right.Right);
+            Assert.Equal(4, result.Left.Left.Value);
+            Assert.Null(result.Left.Right);
+        }
+
+
+        [Fact]
+        public void Serialize_More_Elements_BinaryTree()
+        {
+            var tree = new TreeNode<int>(7)
+            {
+                Left = new TreeNode<int>(5)
+                {
+                    Left = new TreeNode<int>(4),
+                    Right = new TreeNode<int>(6)
+                },
+                Right = new TreeNode<int>(8)
+            };
+
+            var result = new BinaryTreeConverter().Serialize(tree);
+
+            Assert.Equal("7-5-4-null-null-6-null-null-8-null-null-", result);
+        }
+
+        [Fact]
+        public void Deserialize_More_Elements_BinaryTree()
+        {
+            var inputString = "7-5-4-null-null-6-null-null-8-null-null-";
+
+            var result = new BinaryTreeConverter().Deserialize(inputString);
+
+            Assert.Equal(7, result.Value);
+            Assert.Equal(5, result.Left.Value);
+            Assert.Equal(8, result.Right.Value);
+            Assert.Null(result.Right.Left);
+            Assert.Null(result.Right.Right);
+            Assert.Equal(4, result.Left.Left.Value);
+            Assert.Equal(6, result.Left.Right.Value);
+        }
+    }
+}
