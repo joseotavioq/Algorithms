2c1888cf934db511eba08f75cb10fe5ea0aaae6d
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-05-05 16:39:34 -0300

    Algorithm 3-Sum added.

---
+++ src/Algorithms/ArraysAndStrings/ThreeSum.cs
@@ -0,0 +1,73 @@
+﻿using Algorithms.Sorting;
+using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+namespace Algorithms.ArraysAndStrings
+{
+    [MemoryDiagnoser]
+    public class ThreeSum
+    {
+        [Benchmark(Baseline = true)]
+        [Arguments(new int[] { 30, -40, -20, -10, 40, 0, 10, 5 })]
+        public List<int[]> FirstTry(int[] nums)
+        {
+            var list = new List<int[]>();
+
+            if (nums == null || nums.Length < 3)
+                return list;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        if (nums[i] + nums[j] + nums[k] == 0)
+                            list.Add(new int[] { nums[i], nums[j], nums[k] });
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        [Benchmark]
+        [Arguments(new int[] { 30, -40, -20, -10, 40, 0, 10, 5 })]
+        public List<int[]> SecondTry(int[] nums)
+        {
+            var list = new List<int[]>();
+
+            if (nums == null || nums.Length < 3)
+                return list;
+
+            nums = new BubbleSort().ThirdTry(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    int valueToSearchFor = nums[i] + nums[j];
+
+                    int lowIndex = 0;
+                    int highIndex = nums.Length - 1;
+
+                    if (valueToSearchFor >= nums[lowIndex] && valueToSearchFor < nums[highIndex])
+                    {
+                        while (lowIndex <= highIndex)
+                        {
+                            int midIndex = (lowIndex + highIndex) / 2;
+                            if (valueToSearchFor > nums[midIndex])
+                                lowIndex = midIndex + 1;
+                            else if (valueToSearchFor < nums[midIndex])
+                                highIndex = midIndex - 1;
+                            else
+                                list.Add(new int[] { nums[i], nums[j], valueToSearchFor });
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/ThreeSumTest.cs
@@ -0,0 +1,57 @@
+﻿using Algorithms.ArraysAndStrings;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class ThreeSumTest
+    {
+        [Fact]
+        public void FirstTry_Basic_Test()
+        {
+            int[] nums = new int[] { 30, -40, -20, -10, 40, 0, 10, 5 };
+
+            List<int[]> result = new ThreeSum().FirstTry(nums);
+
+            Assert.Equal(4, result.Count);
+            Assert.Equal(new int[] { -40, 0, 40 }, result[0]);
+            Assert.Equal(new int[] { -40, 10, 30 }, result[1]);
+            Assert.Equal(new int[] { -40, 40, 0 }, result[2]);
+            Assert.Equal(new int[] { -10, 0, 10 }, result[3]);
+        }
+
+        [Fact]
+        public void FirstTry_Null_Test()
+        {
+            int[] nums = null;
+
+            List<int[]> result = new ThreeSum().FirstTry(nums);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void SecondTry_Basic_Test()
+        {
+            int[] nums = new int[] { 30, -40, -20, -10, 40, 0, 10, 5 };
+
+            List<int[]> result = new ThreeSum().SecondTry(nums);
+
+            Assert.Equal(4, result.Count);
+            Assert.Equal(new int[] { -40, 0, 40 }, result[0]);
+            Assert.Equal(new int[] { -40, 10, 30 }, result[1]);
+            Assert.Equal(new int[] { -40, 40, 0 }, result[2]);
+            Assert.Equal(new int[] { -10, 0, 10 }, result[3]);
+        }
+
+        [Fact]
+        public void SecondTry_Null_Test()
+        {
+            int[] nums = null;
+
+            List<int[]> result = new ThreeSum().SecondTry(nums);
+
+            Assert.Empty(result);
+        }
+    }
+}
