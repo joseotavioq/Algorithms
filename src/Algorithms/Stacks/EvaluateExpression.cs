825edce008dc2968895b94e2cb096a9f22adaee1
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-05-09 10:17:35 -0300

    Included an algorithm to evaluate string expressions with parentheses.

---
+++ src/Algorithms/Stacks/EvaluateExpression.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Stacks
+{
+    public class EvaluateExpression
+    {
+        public int FirstTry(string expression)
+        {
+            Stack<int> numbers = new Stack<int>();
+            Stack<char> operators = new Stack<char>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '+'
+                    || expression[i] == '-'
+                    || expression[i] == '*'
+                    || expression[i] == '/')
+                    operators.Push(expression[i]);
+                else if (expression[i] >= '0' && expression[i] <= '9')
+                    numbers.Push(expression[i] - '0');
+                else if (expression[i] == ')')
+                {
+                    int b = numbers.Pop();
+                    int a = numbers.Pop();
+                    char op = operators.Pop();
+                    numbers.Push(calculate(a, b, op));
+                }
+            }
+
+            if (numbers.Count == 1)
+                return numbers.Pop();
+            else
+                return 0;
+        }
+
+        private int calculate(int a, int b, char op)
+        {
+            if (op == '+')
+                return a + b;
+            else if (op == '-')
+                return a - b;
+            else if (op == '*')
+                return a * b;
+            else
+                return a / b;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Stacks/EvaluateExpressionTest.cs
@@ -0,0 +1,68 @@
+﻿using Algorithms.Stacks;
+using Xunit;
+
+namespace AlgorithmsTest.Stacks
+{
+    public class EvaluateExpressionTest
+    {
+        [Fact]
+        public void FirstTry_Easy_Expression_With_Sum()
+        {
+            var eval = new EvaluateExpression();
+
+            int result = eval.FirstTry("(1+2)");
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void FirstTry_Easy_Expression_With_Spaces()
+        {
+            var eval = new EvaluateExpression();
+
+            int result = eval.FirstTry("(1 + 2)");
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void FirstTry_Easy_Expression_With_Subtraction()
+        {
+            var eval = new EvaluateExpression();
+
+            int result = eval.FirstTry("(2 - 4)");
+
+            Assert.Equal(-2, result);
+        }
+
+        [Fact]
+        public void FirstTry_Easy_Expression_With_Multiplication()
+        {
+            var eval = new EvaluateExpression();
+
+            int result = eval.FirstTry("(2 * 4)");
+
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void FirstTry_Easy_Expression_With_Division()
+        {
+            var eval = new EvaluateExpression();
+
+            int result = eval.FirstTry("(4 / 2)");
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void FirstTry_Complex_Expression()
+        {
+            var eval = new EvaluateExpression();
+
+            int result = eval.FirstTry("(1 + ((2 + 3) * (4 * 5)))");
+
+            Assert.Equal(101, result);
+        }
+    }
+}
