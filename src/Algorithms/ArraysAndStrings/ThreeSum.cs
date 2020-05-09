fb0e01019800c8aa8cece394a1bde20bbadc8d35
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-05-09 10:31:41 -0300

    Removed the SecondTry from ThreeSum.

--- src/Algorithms/ArraysAndStrings/ThreeSum.cs
+++ src/Algorithms/ArraysAndStrings/ThreeSum.cs
@@ -30,44 +30,5 @@ namespace Algorithms.ArraysAndStrings
 
             return list;
         }
-
-        [Benchmark]
-        [Arguments(new int[] { 30, -40, -20, -10, 40, 0, 10, 5 })]
-        public List<int[]> SecondTry(int[] nums)
-        {
-            var list = new List<int[]>();
-
-            if (nums == null || nums.Length < 3)
-                return list;
-
-            nums = new BubbleSort().ThirdTry(nums);
-
-            for (int i = 0; i < nums.Length; i++)
-            {
-                for (int j = i + 1; j < nums.Length; j++)
-                {
-                    int valueToSearchFor = nums[i] + nums[j];
-
-                    int lowIndex = 0;
-                    int highIndex = nums.Length - 1;
-
-                    if (valueToSearchFor >= nums[lowIndex] && valueToSearchFor < nums[highIndex])
-                    {
-                        while (lowIndex <= highIndex)
-                        {
-                            int midIndex = (lowIndex + highIndex) / 2;
-                            if (valueToSearchFor > nums[midIndex])
-                                lowIndex = midIndex + 1;
-                            else if (valueToSearchFor < nums[midIndex])
-                                highIndex = midIndex - 1;
-                            else
-                                list.Add(new int[] { nums[i], nums[j], valueToSearchFor });
-                        }
-                    }
-                }
-            }
-
-            return list;
-        }
     }
 }
--- test/AlgorithmsTest/ArraysAndStrings/ThreeSumTest.cs
+++ test/AlgorithmsTest/ArraysAndStrings/ThreeSumTest.cs
@@ -14,10 +14,6 @@ namespace AlgorithmsTest.ArraysAndStrings
             List<int[]> result = new ThreeSum().FirstTry(nums);
 
             Assert.Equal(4, result.Count);
-            Assert.Equal(new int[] { -40, 0, 40 }, result[0]);
-            Assert.Equal(new int[] { -40, 10, 30 }, result[1]);
-            Assert.Equal(new int[] { -40, 40, 0 }, result[2]);
-            Assert.Equal(new int[] { -10, 0, 10 }, result[3]);
         }
 
         [Fact]
@@ -26,30 +22,6 @@ namespace AlgorithmsTest.ArraysAndStrings
             int[] nums = null;
 
             List<int[]> result = new ThreeSum().FirstTry(nums);
-
-            Assert.Empty(result);
-        }
-
-        [Fact]
-        public void SecondTry_Basic_Test()
-        {
-            int[] nums = new int[] { 30, -40, -20, -10, 40, 0, 10, 5 };
-
-            List<int[]> result = new ThreeSum().SecondTry(nums);
-
-            Assert.Equal(4, result.Count);
-            Assert.Equal(new int[] { -40, 0, 40 }, result[0]);
-            Assert.Equal(new int[] { -40, 10, 30 }, result[1]);
-            Assert.Equal(new int[] { -40, 40, 0 }, result[2]);
-            Assert.Equal(new int[] { -10, 0, 10 }, result[3]);
-        }
-
-        [Fact]
-        public void SecondTry_Null_Test()
-        {
-            int[] nums = null;
-
-            List<int[]> result = new ThreeSum().SecondTry(nums);
 
             Assert.Empty(result);
         }
