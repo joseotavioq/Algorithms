3f3a8d836e1a3c1f7ebcc4cd58b710b0b5e0a10e
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-05-14 16:09:49 -0300

    Robot in a grid algorithm.

---
+++ src/Algorithms/Recursion/RobotGrid.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Recursion
+{
+    public class RobotGrid
+    {
+        public class Point
+        {
+            public Point(int r, int c)
+            {
+                row = r;
+                column = c;
+            }
+
+            public int row { get; set; }
+            public int column { get; set; }
+        }
+
+        public List<Point> FindPath(bool[][] grid)
+        {
+            if (grid == null || grid.Length == 0)
+                return null;
+
+            List<Point> path = new List<Point>();
+            if (FindPath(path, grid, grid.Length - 1, grid[0].Length - 1))
+                return path;
+
+            return null;
+        }
+
+        private bool FindPath(List<Point> path, bool[][] grid, int row, int col)
+        {
+            if (row < 0 || col < 0 || !grid[row][col])
+                return false;
+
+            if ((row == 0 && col == 0) || FindPath(path, grid, row, col - 1) || FindPath(path, grid, row - 1, col))
+            {
+                path.Add(new Point(row, col));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Recursion/RobotGridTest.cs
@@ -0,0 +1,23 @@
+﻿using Algorithms.Recursion;
+using Xunit;
+
+namespace AlgorithmsTest.Recursion
+{
+    public class RobotGridTest
+    {
+        [Fact]
+        public void FindPath()
+        {
+            bool[][] input = new bool[][] {
+                new bool[] { true, true, false, true },
+                new bool[] { true, true, true, true },
+                new bool[] { false, true, false, true },
+                new bool[] { true, true, true, true }
+            };
+
+            var result = new RobotGrid().FindPath(input);
+
+            Assert.Equal(7, result.Count);
+        }
+    }
+}
