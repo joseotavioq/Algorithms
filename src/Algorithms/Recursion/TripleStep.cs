88283b804b5fd7022ea70b63c8b1b413e2c94e50
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-05-14 12:55:30 -0300

    TripleStep algorithm

---
+++ src/Algorithms/Recursion/TripleStep.cs
@@ -0,0 +1,15 @@
+﻿namespace Algorithms.Recursion
+{
+    public class TripleStep
+    {
+        public int Recursive(int nSteps)
+        {
+            if (nSteps < 0)
+                return 0;
+            else if (nSteps == 0)
+                return 1;
+
+            return Recursive(nSteps - 1) + Recursive(nSteps - 2) + Recursive(nSteps - 3);
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Recursion/TripleStepTest.cs
@@ -0,0 +1,24 @@
+﻿using Algorithms.Recursion;
+using Xunit;
+
+namespace AlgorithmsTest.Recursion
+{
+    public class TripleStepTest
+    {
+        [Fact]
+        public void Steps_Number_4()
+        {
+            var result = new TripleStep().Recursive(4);
+
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void Steps_Number_7()
+        {
+            var result = new TripleStep().Recursive(7);
+
+            Assert.Equal(44, result);
+        }
+    }
+}
