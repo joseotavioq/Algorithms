28c78f34db48da3923ae96ac94fa62a846cae98d
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-05-14 18:05:32 -0300

    Valid Parentheses algorithm.

---
+++ src/Algorithms/Stacks/ValidParentheses.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Stacks
+{
+    public class ValidParentheses
+    {
+        public bool FirstTry(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (stack.Count > 0)
+                {
+                    char last = stack.Peek();
+
+                    if ((last == '(' && c == ')')
+                        || (last == '{' && c == '}')
+                        || (last == '[' && c == ']'))
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+
+                    if (last == '(' || last == '{' || last == '[')
+                        if (c == ')' || c == '}' || c == ']')
+                            return false;
+                }
+
+                stack.Push(c);
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Stacks/ValidParenthesesTest.cs
@@ -0,0 +1,78 @@
+﻿using Algorithms.Stacks;
+using Xunit;
+
+namespace AlgorithmsTest.Stacks
+{
+    public class ValidParenthesesTest
+    {
+        [Fact]
+        public void Only_One_Char()
+        {
+            var input = "(";
+
+            var result = new ValidParentheses().FirstTry(input);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Only_With_Parentheses()
+        {
+            var input = "()";
+
+            var result = new ValidParentheses().FirstTry(input);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Using_The_Three_Different_Forms()
+        {
+            var input = "()[]{}";
+
+            var result = new ValidParentheses().FirstTry(input);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Simple_But_Incorrect()
+        {
+            var input = "(]";
+
+            var result = new ValidParentheses().FirstTry(input);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Complex_But_Incorrect()
+        {
+            var input = "([)]";
+
+            var result = new ValidParentheses().FirstTry(input);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Complex_And_Correct()
+        {
+            var input = "{[]}";
+
+            var result = new ValidParentheses().FirstTry(input);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Complete()
+        {
+            var input = "{[()]}";
+
+            var result = new ValidParentheses().FirstTry(input);
+
+            Assert.True(result);
+        }
+    }
+}
