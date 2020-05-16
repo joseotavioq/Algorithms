0c6edb8c3e39e807323e37e62ab1f91b002afb25
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-05-16 16:47:07 -0300

    Find and Replace algorithm.

---
+++ src/Algorithms/ArraysAndStrings/FindAndReplace.cs
@@ -0,0 +1,72 @@
+﻿namespace Algorithms.ArraysAndStrings
+{
+    public class FindAndReplace
+    {
+        public string FirstTry(string input, string find, string replace)
+        {
+            if (input == null)
+                return null;
+
+            if (find == null || find == "")
+                return null;
+
+            if (replace == null)
+                return null;
+
+            char[] temp = new char[input.Length];
+
+            int j = 0;
+            int tmpIndex = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+
+                if (c == find[j])
+                {
+                    if (j == find.Length - 1)
+                    {
+                        foreach (char r in replace)
+                        {
+                            if (tmpIndex >= temp.Length)
+                                resizeArray(ref temp, temp.Length * 2);
+                            temp[tmpIndex++] = r;
+                        }
+                        j = 0;
+                    }
+                    else
+                        j++;
+                }
+                else if (j > 0)
+                {
+                    while (j >= 0)
+                    {
+                        if (tmpIndex >= temp.Length)
+                            resizeArray(ref temp, temp.Length * 2);
+                        temp[tmpIndex++] = input[i - j];
+                        j--;
+                    }
+                    j = 0;
+                }
+                else
+                {
+                    if (tmpIndex >= temp.Length)
+                        resizeArray(ref temp, temp.Length * 2);
+                    temp[tmpIndex++] = c;
+                }
+            }
+
+            return new string(temp, 0, tmpIndex);
+        }
+
+        private void resizeArray(ref char[] temp, int newSize)
+        {
+            char[] newArray = new char[newSize];
+
+            for (int i = 0; i < temp.Length; i++)
+                newArray[i] = temp[i];
+
+            temp = newArray;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/FindAndReplaceTest.cs
@@ -0,0 +1,128 @@
+﻿using Algorithms.ArraysAndStrings;
+using Xunit;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class FindAndReplaceTest
+    {
+        [Fact]
+        public void Replacing_The_Last_Word()
+        {
+            string input = "I love my country";
+            string find = "country";
+            string replace = "nation";
+
+            var result = new FindAndReplace().FirstTry(input, find, replace);
+
+            Assert.Equal("I love my nation", result);
+        }
+
+        [Fact]
+        public void Replacing_The_Last_Word_But_With_A_Char_In_The_End()
+        {
+            string input = "I love my country!";
+            string find = "country";
+            string replace = "nation";
+
+            var result = new FindAndReplace().FirstTry(input, find, replace);
+
+            Assert.Equal("I love my nation!", result);
+        }
+
+        [Fact]
+        public void Replacing_One_Word()
+        {
+            string input = "1cou ntry I love my country!";
+            string find = "country";
+            string replace = "nation";
+
+            var result = new FindAndReplace().FirstTry(input, find, replace);
+
+            Assert.Equal("1cou ntry I love my nation!", result);
+        }
+
+        [Fact]
+        public void Replacing_Two_Words()
+        {
+            string input = "1country I love my country!";
+            string find = "country";
+            string replace = "nation";
+
+            var result = new FindAndReplace().FirstTry(input, find, replace);
+
+            Assert.Equal("1nation I love my nation!", result);
+        }
+
+        [Fact]
+        public void Replacing_The_Same_Word_Together()
+        {
+            string input = "countrycountry";
+            string find = "country";
+            string replace = "nation";
+
+            var result = new FindAndReplace().FirstTry(input, find, replace);
+
+            Assert.Equal("nationnation", result);
+        }
+
+        [Fact]
+        public void Input_Is_Null()
+        {
+            string input = null;
+            string find = "country";
+            string replace = "nation";
+
+            var result = new FindAndReplace().FirstTry(input, find, replace);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Find_Is_Null()
+        {
+            string input = "I love my country!";
+            string find = null;
+            string replace = "nation";
+
+            var result = new FindAndReplace().FirstTry(input, find, replace);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Replace_Is_Null()
+        {
+            string input = "I love my country!";
+            string find = "country";
+            string replace = null;
+
+            var result = new FindAndReplace().FirstTry(input, find, replace);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Replace_Is_StringEmpty()
+        {
+            string input = "I love my country!";
+            string find = "country";
+            string replace = string.Empty;
+
+            var result = new FindAndReplace().FirstTry(input, find, replace);
+
+            Assert.Equal("I love my !", result);
+        }
+
+        [Fact]
+        public void Replace_To_A_Greater_String()
+        {
+            string input = "I love my country!";
+            string find = "country";
+            string replace = "beautiful home";
+
+            var result = new FindAndReplace().FirstTry(input, find, replace);
+
+            Assert.Equal("I love my beautiful home!", result);
+        }
+    }
+}
