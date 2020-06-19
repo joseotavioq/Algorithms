ae3ca01f24e3822b88f25ca4ac50b4c9912a16ec
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-06-19 18:39:08 -0300

    - Added Newtonsoft.Json package - Added an algorithm to parse a JSON object and create a table based on its values

---
+++ src/Algorithms/ArraysAndStrings/ParseAndTablefy.cs
@@ -0,0 +1,116 @@
+﻿using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace Algorithms.ArraysAndStrings
+{
+    public class ParseAndTablefy
+    {
+        public (Dictionary<string, List<object>>, string) Execute(JToken currentObject)
+        {
+            Dictionary<string, List<object>> elementsParsed = new Dictionary<string, List<object>>();
+
+            parse(currentObject, elementsParsed, "root");
+
+            var stringResult = tablefy(elementsParsed);
+
+            return (elementsParsed, stringResult);
+        }
+
+        private void parse(JToken currentObject, Dictionary<string, List<object>> elementsParsed, string elementName)
+        {
+            if (currentObject == null)
+            {
+                return;
+            }
+            else if (currentObject.Type == JTokenType.Array || currentObject.Type == JTokenType.Object)
+            {
+                foreach (var child in currentObject.Children())
+                {
+                    parse(child, elementsParsed, elementName);
+                }
+            }
+            else if (currentObject.Type == JTokenType.Property)
+            {
+                JProperty property = currentObject as JProperty;
+                string propName = (!string.IsNullOrEmpty(elementName)) ? $"{elementName}.{property.Name}" : property.Name;
+                parse(property.Value, elementsParsed, propName);
+            }
+            else
+            {
+                JValue value = currentObject as JValue;
+
+                if (!elementsParsed.ContainsKey(elementName))
+                {
+                    elementsParsed.Add(elementName, new List<object>() { value.Value });
+                }
+                else
+                {
+                    elementsParsed[elementName].Add(value.Value);
+                }
+            }
+        }
+
+        private string tablefy(Dictionary<string, List<object>> elementsParsed)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //Print Header
+            int columnIndex = 0;
+            foreach (var item in elementsParsed)
+            {
+                sb.Append(item.Key);
+                if (columnIndex < elementsParsed.Count)
+                {
+                    sb.Append(" | ");
+                }
+                columnIndex++;
+            }
+
+            sb.AppendLine();
+            sb.AppendLine(string.Empty.PadRight(sb.Length, '-'));
+
+            //Print Rows
+            int rowsLength = getMaxRowLength(elementsParsed);
+
+            for (int rowIndex = 0; rowIndex < rowsLength; rowIndex++)
+            {
+                columnIndex = 0;
+                foreach (var item in elementsParsed)
+                {
+                    if (rowIndex >= item.Value.Count)
+                    {
+                        sb.Append(item.Value[item.Value.Count - 1]);
+                    }
+                    else
+                    {
+                        sb.Append(item.Value[rowIndex]);
+                    }
+
+                    if (columnIndex < elementsParsed.Count)
+                    {
+                        sb.Append(" | ");
+                        columnIndex++;
+                    }
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
+        private int getMaxRowLength(Dictionary<string, List<object>> elementsParsed)
+        {
+            int max = 0;
+
+            foreach (var item in elementsParsed)
+            {
+                if (item.Value.Count > max)
+                    max = item.Value.Count;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/ParseAndTablefyTest.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Algorithms.ArraysAndStrings;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class ParseAndTablefyTest
+    {
+        [Fact]
+        public void Parse_And_Tablefy_A_Simple_Json()
+        {
+            string json = @"[
+                               {
+                                  'account':'abc123',
+                                  'date':'2020-11-06T00:00:00Z',
+                                  'transactions':
+                                  [
+                                     {
+                                        'value':45
+                                     },
+                                     {
+                                        'value':10
+                                     },
+                                     {
+                                        'value':22
+                                     },
+                                     {
+                                        'value':43
+                                     }
+                                  ],
+                                  'information':
+                                  {
+                                     'name':'ABC DEF',
+                                     'age':33
+                                  }
+                               }
+                            ]";
+
+            JToken parsedValue = JsonConvert.DeserializeObject(json, new JsonSerializerSettings
+            {
+                DateParseHandling = DateParseHandling.None
+            }) as JToken;
+
+            var (result, stringResult) = new ParseAndTablefy().Execute(parsedValue.Root);
+
+            Assert.Equal(5, result.Count);
+            Assert.Equal("abc123", result["root.account"][0]);
+            Assert.Equal("2020-11-06T00:00:00Z", result["root.date"][0]);
+            Assert.Equal("ABC DEF", result["root.information.name"][0]);
+            Assert.Equal((long)33, result["root.information.age"][0]);
+
+            Assert.Equal((long)45, result["root.transactions.value"][0]);
+            Assert.Equal((long)10, result["root.transactions.value"][1]);
+            Assert.Equal((long)22, result["root.transactions.value"][2]);
+            Assert.Equal((long)43, result["root.transactions.value"][3]);
+
+            Assert.Equal(@"root.account | root.date | root.transactions.value | root.information.name | root.information.age | 
+------------------------------------------------------------------------------------------------------
+abc123 | 2020-11-06T00:00:00Z | 45 | ABC DEF | 33 | 
+abc123 | 2020-11-06T00:00:00Z | 10 | ABC DEF | 33 | 
+abc123 | 2020-11-06T00:00:00Z | 22 | ABC DEF | 33 | 
+abc123 | 2020-11-06T00:00:00Z | 43 | ABC DEF | 33 | 
+", stringResult);
+        }
+    }
+}
