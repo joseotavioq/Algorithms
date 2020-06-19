f0e9a8a02fb5ec64a6b7ebaaf8d89ec48ca4a3b6
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-06-19 19:21:21 -0300

    Solving error on the returned string

--- test/AlgorithmsTest/ArraysAndStrings/ParseAndTablefyTest.cs
+++ test/AlgorithmsTest/ArraysAndStrings/ParseAndTablefyTest.cs
@@ -57,7 +57,7 @@ namespace AlgorithmsTest.ArraysAndStrings
             Assert.Equal((long)22, result["root.transactions.value"][2]);
             Assert.Equal((long)43, result["root.transactions.value"][3]);
 
-            Assert.Equal(@$"root.account | root.date | root.transactions.value | root.information.name | root.information.age{NEWLINE}---------------------------------------------------------------------------------------------------{NEWLINE}abc123 | 2020-11-06T00:00:00Z | 45 | ABC DEF | 33{NEWLINE}abc123 | 2020-11-06T00:00:00Z | 10 | ABC DEF | 33{NEWLINE}abc123 | 2020-11-06T00:00:00Z | 22 | ABC DEF | 33{NEWLINE}abc123 | 2020-11-06T00:00:00Z | 43 | ABC DEF | 33{NEWLINE}", stringResult);
+            Assert.Equal($"root.account | root.date | root.transactions.value | root.information.name | root.information.age{NEWLINE}--------------------------------------------------------------------------------------------------{NEWLINE}abc123 | 2020-11-06T00:00:00Z | 45 | ABC DEF | 33{NEWLINE}abc123 | 2020-11-06T00:00:00Z | 10 | ABC DEF | 33{NEWLINE}abc123 | 2020-11-06T00:00:00Z | 22 | ABC DEF | 33{NEWLINE}abc123 | 2020-11-06T00:00:00Z | 43 | ABC DEF | 33{NEWLINE}", stringResult);
         }
     }
 }
