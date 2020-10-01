b8ecfa70b91669894afa82dc2aeb9078706ce0fd
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-10-01 16:47:55 -0300

    Adjusting Second Try for Rotate Left algorithm.

--- src/Algorithms/Logic/RotateLeft.cs
+++ src/Algorithms/Logic/RotateLeft.cs
@@ -19,17 +19,14 @@
 
         public int[] SecondTry(int[] array, int numberOfRotations)
         {
-            while (numberOfRotations-- > 0)
+            int[] otherArray = new int[array.Length];
+
+            for (int j = 0; j < array.Length; j++)
             {
-                int tmp = array[0];
-                for (int j = 0; j < array.Length - 1; j++)
-                {
-                    array[j] = array[j + 1];
-                }
-                array[array.Length - 1] = tmp;
+                otherArray[j] = array[(j + numberOfRotations) % array.Length];
             }
 
-            return array;
+            return otherArray;
         }
     }
 }
--- test/AlgorithmsTest/Logic/RotateLeftTest.cs
+++ test/AlgorithmsTest/Logic/RotateLeftTest.cs
@@ -17,6 +17,17 @@ namespace AlgorithmsTest.Logic
         }
 
         [Fact]
+        public void FirstTry_Other_Test()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            int numberOfRotations = 2;
+
+            array = new RotateLeft().FirstTry(array, numberOfRotations);
+
+            Assert.Equal(new int[] { 3, 4, 5, 1, 2 }, array);
+        }
+
+        [Fact]
         public void SecondTry_Basic_Test()
         {
             int[] array = new int[] { 1, 2, 3, 4, 5 };
@@ -25,6 +36,17 @@ namespace AlgorithmsTest.Logic
             array = new RotateLeft().SecondTry(array, numberOfRotations);
 
             Assert.Equal(new int[] { 5, 1, 2, 3, 4 }, array);
+        }
+
+        [Fact]
+        public void SecondTry_Other_Test()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            int numberOfRotations = 2;
+
+            array = new RotateLeft().SecondTry(array, numberOfRotations);
+
+            Assert.Equal(new int[] { 3, 4, 5, 1, 2 }, array);
         }
     }
 }
