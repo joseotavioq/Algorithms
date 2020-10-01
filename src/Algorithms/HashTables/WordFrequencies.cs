9fdcc52a577570782ca6ec6dc78d3538e0920101
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-10-01 16:50:28 -0300

    Added a summary description.

--- src/Algorithms/HashTables/WordFrequencies.cs
+++ src/Algorithms/HashTables/WordFrequencies.cs
@@ -2,6 +2,10 @@
 
 namespace Algorithms.HashTables
 {
+    /// <summary>
+    /// Design a method to find the frequency of occurrences of any given word in a book.
+    /// What if we were running this algorithm multiple times?
+    /// </summary>
     public class WordFrequencies
     {
         private Dictionary<string, int> _wordsCount = null;
