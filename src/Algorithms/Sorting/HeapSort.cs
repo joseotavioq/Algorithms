7740835bfbe13553fedc9ed249d55cfe9687ba85
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-10-16 23:10:55 -0300

    Heap sort implemented.

---
+++ src/Algorithms/Sorting/HeapSort.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Validators;
+
+namespace Algorithms.Sorting
+{
+    public class HeapSort
+    {
+        public int[] FirstTry(int[] values)
+        {
+            //6, 4, 9, 2, 1, 5, 7, 3, 8
+            //         6
+            //        /  \
+            //       4    9
+            //      / \  / \
+            //     2   1 5  7
+            //    / \
+            //   3   8
+
+            int size = values.Length;
+            while (size > 0)
+            {
+                for (int i = size / 2; i >= 0; i--)
+                {
+                    int left = (i * 2) + 1;
+                    if (left < size && values[left] > values[i])
+                    {
+                        swap(values, i, left);
+                    }
+
+                    int right = (i * 2) + 2;
+                    if (right < size && values[right] > values[i])
+                    {
+                        swap(values, i, right);
+                    }
+                }
+
+                swap(values, 0, size - 1);
+                size--;
+            }
+
+            return values;
+        }
+
+        private void swap(int[] values, int i, int j)
+        {
+            int tmp = values[i];
+            values[i] = values[j];
+            values[j] = tmp;
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Sorting/HeapSortTest.cs
@@ -0,0 +1,40 @@
+﻿using Algorithms.Sorting;
+using Xunit;
+
+namespace AlgorithmsTest.Sorting
+{
+    public class HeapSortTest
+    {
+        [Fact]
+        public void FirstTry_Basic_Test()
+        {
+            int[] listOfNumbers = new int[] { 10, 6, 7, 5, 15, 17, 12 };
+
+            var sortedArray = new HeapSort().FirstTry(listOfNumbers);
+
+            Assert.Equal(new int[] { 5, 6, 7, 10, 12, 15, 17 }, sortedArray);
+        }
+
+        [Fact]
+        public void FirstTry_Basic_With_Odd_Size_Test()
+        {
+            int[] listOfNumbers = new int[] { 6, 4, 9, 2, 1, 5, 7, 3, 8 };
+
+            var sortedArray = new HeapSort().FirstTry(listOfNumbers);
+
+            for (int i = 1; i <= sortedArray.Length; i++)
+                Assert.Equal(i, sortedArray[i - 1]);
+        }
+
+        [Fact]
+        public void FirstTry_Basic_With_Even_Size_Test()
+        {
+            int[] listOfNumbers = new int[] { 6, 4, 2, 1, 5, 7, 3, 8 };
+
+            var sortedArray = new HeapSort().FirstTry(listOfNumbers);
+
+            for (int i = 1; i <= sortedArray.Length; i++)
+                Assert.Equal(i, sortedArray[i - 1]);
+        }
+    }
+}
