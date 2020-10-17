1a640d537e100191e5759de6d62b3f6f3294f7a4
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-10-17 13:28:43 -0300

    Find the Kth largest item on an array implemented.

---
+++ src/Algorithms/ArraysAndStrings/FindTheKthLargest.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Algorithms.ArraysAndStrings
+{
+    public class FindTheKthLargest
+    {
+        public int FirstTry(int[] nums, int k)
+        {
+            if (nums == null || nums.Length < k)
+            {
+                return -1;
+            }
+
+            int i = 0;
+            while (i < nums.Length - k)
+            {
+                i = SeparateLessThanPivot(nums, i);
+            }
+
+            return nums[nums.Length - k];
+        }
+
+        private int SeparateLessThanPivot(int[] nums, int i)
+        {
+            int j = i;
+            int pivot = nums.Length - 1;
+
+            while (j < pivot)
+            {
+                if (nums[j] < nums[pivot])
+                {
+                    swap(nums, i, j);
+                    i++;
+                }
+                j++;
+            }
+
+            swap(nums, i, pivot);
+            return i;
+        }
+
+        private void swap(int[] values, int i, int j)
+        {
+            if (i != j)
+            {
+                int tmp = values[i];
+                values[i] = values[j];
+                values[j] = tmp;
+            }
+        }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/FindTheKthLargestTest.cs
@@ -0,0 +1,53 @@
+﻿using Algorithms.ArraysAndStrings;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class FindTheKthLargestTest
+    {
+        [Fact]
+        public void FirstTry_Basic_Test()
+        {
+            int[] nums = new int[] { 3, 2, 1, 5, 6, 4 };
+            int k = 2;
+
+            int result = new FindTheKthLargest().FirstTry(nums, k);
+
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void FirstTry_With_Negative_Values()
+        {
+            int[] nums = new int[] { 30, -40, -20, -10, 40, 0, 10, 5 };
+            int k = 1;
+
+            int result = new FindTheKthLargest().FirstTry(nums, k);
+
+            Assert.Equal(40, result);
+        }
+
+        [Fact]
+        public void FirstTry_Already_Sorted_Array()
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int k = 5;
+
+            int result = new FindTheKthLargest().FirstTry(nums, k);
+
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void FirstTry_Sort_Inverted_Array()
+        {
+            int[] nums = new int[] { 7, 6, 5, 4, 3, 2, 1 };
+            int k = 3;
+
+            int result = new FindTheKthLargest().FirstTry(nums, k);
+
+            Assert.Equal(5, result);
+        }
+    }
+}
