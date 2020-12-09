162b2ea2a3c52fb22a10a8da2026a8876e41803b
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-12-09 10:47:36 -0300

    Implemented algorithm to check if a tree is symetric.

---
+++ src/Algorithms/Tree/IsItSymetric.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms.Tree
+{
+    public class IsItSymetric
+    {
+        public bool FirstTry(TreeNode<int> root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            return isMirror(root.Left, root.Right);
+        }
+
+        private bool isMirror(TreeNode<int> left, TreeNode<int> right)
+        {
+            if (left == null && right == null)
+            {
+                return true;
+            }
+            else if (left == null || right == null)
+            {
+                return false;
+            }
+            else if (left.Value != right.Value)
+            {
+                return false;
+            }
+
+            return isMirror(left.Left, right.Right)
+               && isMirror(left.Right, right.Left);
+        }
+    }
+}
---
+++ test/AlgorithmsTest/Tree/IsItSymetricTest.cs
@@ -0,0 +1,52 @@
+﻿using Algorithms.Tree;
+using Xunit;
+
+namespace AlgorithmsTest.Tree
+{
+    public class IsItSymetricTest
+    {
+        [Fact]
+        public void Symetric_Tree()
+        {
+            var tree = new TreeNode<int>(1);
+
+            tree.Left = new TreeNode<int>(2);
+            tree.Left.Left = new TreeNode<int>(3);
+            tree.Left.Right = new TreeNode<int>(4);
+            tree.Left.Left.Left = new TreeNode<int>(5);
+            tree.Left.Left.Right = new TreeNode<int>(6);
+
+            tree.Right = new TreeNode<int>(2);
+            tree.Right.Right = new TreeNode<int>(3);
+            tree.Right.Left = new TreeNode<int>(4);
+            tree.Right.Right.Right = new TreeNode<int>(5);
+            tree.Right.Right.Left = new TreeNode<int>(6);
+
+            var result = new IsItSymetric().FirstTry(tree);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Assymetric_Tree()
+        {
+            var tree = new TreeNode<int>(1);
+
+            tree.Left = new TreeNode<int>(2);
+            tree.Left.Left = new TreeNode<int>(3);
+            tree.Left.Right = new TreeNode<int>(4);
+            tree.Left.Left.Left = new TreeNode<int>(5);
+            tree.Left.Left.Right = new TreeNode<int>(6);
+
+            tree.Right = new TreeNode<int>(2);
+            tree.Right.Right = new TreeNode<int>(3);
+            tree.Right.Left = new TreeNode<int>(4);
+            tree.Right.Right.Right = new TreeNode<int>(6);
+            tree.Right.Right.Left = new TreeNode<int>(5);
+
+            var result = new IsItSymetric().FirstTry(tree);
+
+            Assert.False(result);
+        }
+    }
+}
