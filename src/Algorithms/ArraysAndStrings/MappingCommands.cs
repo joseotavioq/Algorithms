f282e3b0dcf93d2ac9a24b9f940d878c4e035944
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-12-09 11:20:54 -0300

    Implemented an algorithm to translate a word in commands.

---
+++ src/Algorithms/ArraysAndStrings/MappingCommands.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.ArraysAndStrings
+{
+    public class MappingCommands
+    {
+        public Dictionary<char, Position> _mappings = new Dictionary<char, Position>();
+
+        public MappingCommands()
+        {
+            LoadMappers();
+        }
+
+        private void LoadMappers()
+        {
+            for (int i = 'a'; i <= 'z'; i++)
+            {
+                int j = i - 'a';
+                _mappings.Add((char)i, new Position(j, j));
+            }
+        }
+
+        public Commands[] GetCommands(string words)
+        {
+            if (words == null)
+            {
+                return null;
+            }
+
+            List<Commands> commands = new List<Commands>();
+
+            Position currentPosition = new Position(0, 0);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char c = words[i];
+                Position position = _mappings[c];
+
+                while (currentPosition.X < position.X)
+                {
+                    currentPosition.X++;
+                    commands.Add(Commands.Right);
+                }
+
+                while (currentPosition.Y < position.Y)
+                {
+                    currentPosition.Y++;
+                    commands.Add(Commands.Down);
+                }
+
+                while (currentPosition.X > position.X)
+                {
+                    currentPosition.X--;
+                    commands.Add(Commands.Left);
+                }
+
+                while (currentPosition.Y > position.Y)
+                {
+                    currentPosition.Y--;
+                    commands.Add(Commands.Up);
+                }
+
+                commands.Add(Commands.Click);
+            }
+
+            return commands.ToArray();
+        }
+    }
+    public enum Commands
+    {
+        Right,
+        Left,
+        Down,
+        Up,
+        Click
+    }
+
+    public class Position
+    {
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/MappingCommandsTest.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.ArraysAndStrings;
+using Xunit;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class MappingCommandsTest
+    {
+        [Fact]
+        public void Say_Hello()
+        {
+            string word = "hello";
+
+            var commands = new MappingCommands().GetCommands(word);
+
+            Assert.Equal(45, commands.Length);
+            Assert.Equal(Commands.Click, commands[commands.Length - 1]);
+        }
+    }
+}
