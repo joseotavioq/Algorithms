ad58634f51dc11154cbd4a5a05acb90aa13dfabb
Jose Otavio Quaglio <joseotavio@gmail.com>
2020-12-09 11:53:14 -0300

    Implemented an algorithm to print a word with separators and added below a string with a char informing a cursor position.

---
+++ src/Algorithms/ArraysAndStrings/PrintWordWithSeparator.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Algorithms.ArraysAndStrings
+{
+    public class PrintWordWithSeparator
+    {
+        public string Print(string word, char separator, int cursorPosition)
+        {
+            if (word == null || cursorPosition > word.Length)
+            {
+                return null;
+            }
+
+            StringBuilder firstLine = new StringBuilder(word.Length * 2 - 1);
+            StringBuilder secondLine = new StringBuilder(word.Length * 2 - 1);
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                firstLine.Append(word[i]);
+
+                if (i < word.Length - 1)
+                {
+                    if (i + 1 == cursorPosition)
+                    {
+                        secondLine.Append('^');
+                    }
+                    else
+                    {
+                        secondLine.Append('-');
+                    }
+
+                    firstLine.Append(separator);
+                }
+
+                secondLine.Append('-');
+            }
+
+            return firstLine.ToString() + secondLine.ToString();
+        }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/PrintWordWithSeparatorTest.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms.ArraysAndStrings;
+using Xunit;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class PrintWordWithSeparatorTest
+    {
+        [Fact]
+        public void Print_Hello_Word()
+        {
+            string word = "hello";
+
+            var result = new PrintWordWithSeparator().Print(word, ',', 2);
+
+            string expectedResult = @"h,e,l,l,o--^------";
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
