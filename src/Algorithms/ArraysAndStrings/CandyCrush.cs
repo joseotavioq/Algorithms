8ef1c781b8ae67178d661437bd9672414c1c0f68
Jose Otavio Quaglio <joseotavio@gmail.com>
2021-01-06 18:16:22 -0300

    Implemented a candy crush like algorithm.

---
+++ src/Algorithms/ArraysAndStrings/CandyCrush.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.ArraysAndStrings
+{
+    public class CandyCrush
+    {
+        public void FirstTry(List<int> numbers)
+        {
+            if (numbers == null || numbers.Count == 0)
+            {
+                return;
+            }
+
+            int lastNumber = -1;
+            int countOfEquals = 1;
+            int i = 0;
+            int lastBeforeRemoved = 0;
+            bool elementsRemoved = false;
+            while (i < numbers.Count)
+            {
+                if (numbers[i] == lastNumber)
+                {
+                    if (countOfEquals == 1)
+                    {
+                        lastBeforeRemoved = i - 2;
+                    }
+
+                    countOfEquals++;
+                }
+                else if (countOfEquals > 1)
+                {
+                    if (countOfEquals >= 3)
+                    {
+                        RemoveElements(numbers, countOfEquals, i);
+
+                        i = lastBeforeRemoved;
+                        while (i > 0 && numbers[i] == numbers[i - 1])
+                        {
+                            i--;
+                        }
+
+                        elementsRemoved = true;
+                    }
+
+                    countOfEquals = 1;
+                    lastNumber = -1;
+                }
+
+                if (!elementsRemoved)
+                {
+                    lastNumber = numbers[i];
+                    i++;
+                }
+                else
+                {
+                    elementsRemoved = false;
+                }
+            }
+
+            if (countOfEquals >= 3)
+            {
+                RemoveElements(numbers, countOfEquals, i);
+            }
+        }
+
+        private void RemoveElements(List<int> numbers, int countOfEquals, int i)
+        {
+            for (int j = i - 1; j >= i - countOfEquals; j--)
+            {
+                numbers.RemoveAt(j);
+            }
+        }
+    }
+}
---
+++ test/AlgorithmsTest/ArraysAndStrings/CandyCrushTest.cs
@@ -0,0 +1,33 @@
+﻿using Algorithms.ArraysAndStrings;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AlgorithmsTest.ArraysAndStrings
+{
+    public class CandyCrushTest
+    {
+        [Fact]
+        public void Removing_Three_Combinations()
+        {
+            List<int> numbers = new List<int>() { 4, 2, 1, 1, 1, 2, 3, 3, 3, 2, 2, 2 };
+
+            new CandyCrush().FirstTry(numbers);
+
+            Assert.Single(numbers);
+            Assert.Equal(4, numbers[0]);
+        }
+
+        [Fact]
+        public void Removing_Two_Combinations()
+        {
+            List<int> numbers = new List<int>() { 4, 2, 1, 1, 1, 2, 3, 3, 3 };
+
+            new CandyCrush().FirstTry(numbers);
+
+            Assert.Equal(3, numbers.Count);
+            Assert.Equal(4, numbers[0]);
+            Assert.Equal(2, numbers[1]);
+            Assert.Equal(2, numbers[2]);
+        }
+    }
+}
